ecc962520376dc4b0a90fa59f7bbd77f9ada54d0
vittorionegri68-alt <vittorionegri68@gmail.com>
2026-04-28 08:55:19 +0200

    Update address in contactInfo

--- src/config/contact_info.js
+++ src/config/contact_info.js
@@ -1,7 +1,7 @@
 export const contactInfo = {
   phone: '+39 392 520 0650',
   email: 'luceacollection@gmail.com',
-  address: 'Corso C.B. Cavour 75, 47521 Cesena FC, Italy',
+  address: 'Corso Garibaldi 75, 47521 Cesena FC, Italy',
   instagram: 'https://www.instagram.com/luceacollection_/',
   web_it: 'https://romagna-affitti-brevi.it/',
   web_en: 'https://romagna-short-stay.com/',
